0fafe09bf2fed405292d4332fa5b70149be85ccd
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-06-11 11:20:55 -0700

    Adding ajax call for yelp and marking locations around user with blue pins on map within yelp.js

--- yelp.js
+++ yelp.js
@@ -3,7 +3,7 @@ var pos = {};
 
 function initMap() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position){
+        navigator.geolocation.getCurrentPosition(function (position) {
             pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
@@ -16,7 +16,7 @@ function initMap() {
                 },
                 zoom: 15
             });
-            
+
             var marker = new google.maps.Marker({
                 position: pos,
                 map: map,
@@ -24,6 +24,30 @@ function initMap() {
                 icon: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"
             })
             marker.setMap(map);
+            var queryURL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude=' + pos.lat + '&longitude=' + pos.lng + '&term=public+restroom&reviews&radius=10000&limit=15&attributes=gender_neutral_restrooms';
+            console.log(pos.lat);
+            console.log(pos.lng);
+            $.ajax({
+                url: queryURL,
+                method: "GET",
+                headers: {
+                    authorization: "Bearer 4Rm7FqyoBh0DGVD6bV936T1y38wYSXyOiQBtQsIza6j_MZVWcPuLtT7x_06Ej7j5TN4ZFgsOAxlj_FHlQrjgyfYbXsGuYjQeamj84ii533Ii5sTH4wKUUjhqNqf6XHYx"
+                }
+            }).then(function (response) {
+                for (var i = 0; i < response.businesses.length; i++) {
+                    var yelpPos = {
+                        lat: response.businesses[i].coordinates.latitude,
+                        lng: response.businesses[i].coordinates.longitude
+                    };
+                    var marker = new google.maps.Marker({
+                        position: yelpPos,
+                        map: map,
+                        title: response.businesses[i].alias,
+                        icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+                    });
+                    marker.setMap(map);
+                }
+            })
         })
     }
 }

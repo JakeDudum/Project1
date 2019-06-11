720adfcb4a6c74b6fdb51c36d4a41758440ef10f
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-06-11 11:10:00 -0700

    Adding more styling to test.html. Finishing initMap function to display google map with marker indicating user's location in yelp.js

--- yelp.js
+++ yelp.js
@@ -2,10 +2,43 @@ var map, infoWindow;
 var pos = {};
 
 function initMap() {
-    if(navigator.geolocation.getCurrentPosition(function (position) {
-        pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-        };
-    })
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position){
+            pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            }
+
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {
+                    lat: pos.lat,
+                    lng: pos.lng
+                },
+                zoom: 15
+            });
+            
+            var marker = new google.maps.Marker({
+                position: pos,
+                map: map,
+                title: "You are here.",
+                icon: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"
+            })
+            marker.setMap(map);
+        })
+    }
 }
+
+// function mapIt(position) {
+//     pos = {
+//         lat: position.coords.latitude,
+//         lng: position.coords.longitude
+//     };
+
+//     map = new google.maps.Map($("#map"), {
+//         center: {
+//             lat: pos.lat,
+//             lng: pos.lng
+//         },
+//         zoom: 15
+//     });
+// };
